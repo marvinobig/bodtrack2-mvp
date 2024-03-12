08ba6e66f686590d45410e32c90c9de6111b6547
marvinobig <88102362+marvinobig@users.noreply.github.com>
2024-03-12 17:50:53 +0000

    change index page title

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ $current_date = date("l jS, F Y");
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Example Project Directory</title>
+        <title>Bodtrack 2</title>
     </head>
 
     <body>
